c3ae3ff45c22cbb2888a336ca352274cca8d8a31
Miaroaming <mianzroaming@gmail.com>
2024-12-04 09:47:31 +1300

    updated file naming

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import ContactForm from '../components/Contactform'
+import ContactForm from '../components/ContactForm'
 
 
 const About = () => {
